7a487437daea15c4b9e6270c530da7328f397345
Rohit Das Gupta <rohitguptarg6542@gmail.com>
2025-08-29 16:48:38 +0530

    commit5

---
+++ Java/CommandLineDemo.java
@@ -0,0 +1,18 @@
+/* Java Program to pass Command Line Arguments for Input */
+
+class CommandLineDemo {
+	public static void main(String[] args)
+	{
+		int number1, number2, sum;
+		String name;
+
+		name = args[0];
+		number1 = Integer.parseInt(args[1]);  //by default args is String type
+		number2 = Integer.parseInt(args[2]);
+
+		sum= number1 + number2;
+	
+		System.out.println(name+ " is  a Java Programmer");
+		System.out.println("The Sum of 2 Numbers is: "+sum);
+	}
+}
---
+++ Java/SumAverage.java
@@ -0,0 +1,16 @@
+class SumAverage
+{
+	public static void main(String[] args) 
+	{
+		int a=20, b=45, c=30, sum;
+		float avg;
+
+		sum = a+b+c;
+		avg=(float)sum/3;  //Typecasting
+
+		System.out.println("The Sum of 3 Numbers is :" +sum);
+		System.out.println("The Average of 3 Numbers is :" +avg);
+
+	}
+}
+		
